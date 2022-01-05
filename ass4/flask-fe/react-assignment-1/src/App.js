22ac229c28d40893fa39321ad5991f681332bc9a
daveFrizky <frizkydavid@gmail.com>
2022-01-05 09:14:14 +0700

    update

--- ass4/flask-fe/react-assignment-1/src/App.js
+++ ass4/flask-fe/react-assignment-1/src/App.js
@@ -12,10 +12,6 @@ function App() {
   },[]);
   return (
     <div className="App">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-      rel="stylesheet" 
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
-      crossorigin="anonymous"></link>
       <nav className='navbar sticky-top' style={{backgroundColor:'#e3f2fd',height:120}}>
         <h1 style={{marginLeft:160,fontStyle:'oblique'}}><b>React App</b></h1>
       </nav>
@@ -27,7 +23,7 @@ function App() {
             <div className='isi '>
               <h2 style={{color:"#3b6ac9"}}>{data.title}</h2>
               {data.body}<br/><br/>
-              {data.date}<br/>
+              {Date(data.date)}<br/>
               <hr/>
             </div>
           ))}
